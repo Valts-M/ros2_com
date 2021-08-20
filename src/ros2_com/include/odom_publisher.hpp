e89ac7142fc0b34f7bbd6670162107550b04762b
Valts-M <valts114@gmail.com>
2021-08-20 11:11:17 +0000

    aaaa

--- src/ros2_com/include/odom_publisher.hpp
+++ src/ros2_com/include/odom_publisher.hpp
@@ -6,11 +6,43 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "nav_msgs/msg/odometry.hpp"
+#include "shmem/shmem_consumer.hpp"
 
 using namespace std::chrono_literals;
 
 namespace ros2_com
 {
+/*!
+* @brief Data structure for containing reactd log data
+*/
+struct ReactdLog
+{
+  /*!
+    * @brief Timestamp that corresponds to the measurements
+  */
+  double ts{0.0};
+  /*!
+    * @brief Gyroscope measurement about Z axis
+  */
+  int gyroZ{0};
+  /*!
+    * @brief VectorNAV gyroscope measurement (not used)
+  */
+  double vGyroZ{0.0};
+  /*!
+    * @brief Left encoder sensor's measurement
+  */
+  int leftEncoder{0};
+  /*!
+    * @brief Right encoder sensor's measurement
+  */
+  int rightEncoder{0};
+  /*!
+    * @brief The id of the measurements
+  */
+  size_t id{0U};
+};
+
 class OdometryPublisher : public rclcpp::Node
 {
 public:
@@ -21,38 +53,9 @@ private:
   void timer_callback();
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<nav_msgs::msg::Odometry>::SharedPtr publisher_;
+  std::unique_ptr<ShmemConsumer<ReactdLog, Storage>> m_odomConsumer;
   size_t count_;
   bool reconnect();
-
-  /*!
-  * @brief Data structure for containing reactd log data
-  */
-  struct ReactdLog
-  {
-    /*!
-     * @brief Timestamp that corresponds to the measurements
-    */
-    double ts{0.0};
-    /*!
-     * @brief Gyroscope measurement about Z axis
-    */
-    int gyroZ{0};
-    /*!
-     * @brief VectorNAV gyroscope measurement (not used)
-    */
-    double vGyroZ{0.0};
-    /*!
-     * @brief Left encoder sensor's measurement
-    */
-    int leftEncoder{0};
-    /*!
-     * @brief Right encoder sensor's measurement
-    */
-    int rightEncoder{0};
-    /*!
-     * @brief The id of the measurements
-    */
-    size_t id{0U};
   };
 };
 }
