b1e1660db579f79dd984ac647bf04e6793430c84
Valts-M <valts114@gmail.com>
2021-08-20 10:59:12 +0000

    OdometryPublisher

--- src/ros2_com/src/odom_publisher.cpp
+++ src/ros2_com/src/odom_publisher.cpp
@@ -15,13 +15,52 @@ namespace ros2_com
       500ms, std::bind(&OdometryPublisher::timer_callback, this));
     }
 
-    void OdometryPublisher::timer_callback()
+    void OdometryPublisher::timerCallback()
     {
-      // auto message = std_msgs::msg::String();
-      // message.data = "Hello, world! " + std::to_string(count_++);
-      // RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      // publisher_->publish(message);
+      if (!reconnect()) return;
+
+      {
+          std::lock_guard<std::mutex> lock(m_data.mtx);
+          if (!m_data.newData)return;
+          std::swap(m_data.front, m_data.back);
+          m_data.newData = false;
+      }
+
+      auto message = std_msgs::msg::String();
+      message.data = m_data.front;
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      m_publisher->publish(message);
+
     }
+
+  bool OdometryPublisher::reconnect()
+	{
+		if (!m_odomConsumer.get())
+		{
+			m_odomConsumer = std::make_unique<ShmemConsumer<OdomData, Storage>>("ros_odometry", "robot_odometry", "");
+			return false;
+		}
+
+		if (!m_odomConsumer->isObjectFound()) m_odomConsumer->findObject();
+
+		//odomLogTask();
+		return true;
+	}
+
+    // bool OdometryPublisher::odomLogTask()
+    // {
+    // 	if (!m_odomConsumer.get()) return false;
+    // 	if (m_odomConsumer->isAsyncWaiting()) return true;
+
+    // 	const bool waiting = m_odomConsumer->asyncWait(
+    // 		[&](const int32_t& t_input)
+    // 		{
+    // 			std::lock_guard<std::mutex> lock(m_data.mtx);
+    // 			m_data.back = t_input;
+    // 			m_data.newData = true;
+    // 		});
+    // 	return m_odomConsumer->isAsyncWaiting();
+    // }
 }
 
 int main(int argc, char * argv[])
