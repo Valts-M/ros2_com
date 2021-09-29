5d2d69b26a48b9d8164a7725913c0432cb444521
Valts-M <valts114@gmail.com>
2021-09-29 11:37:15 +0000

    stuff

--- src/ros2_com/launch/play_bag.launch.py
+++ src/ros2_com/launch/play_bag.launch.py
@@ -7,7 +7,6 @@ from launch_ros.actions import Node
 launch.Event
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time')
     slam_params_file = LaunchConfiguration('slam_params_file')
     bag_file = LaunchConfiguration('bag_file')
 
@@ -25,7 +24,7 @@ def generate_launch_description():
         executable='async_slam_toolbox_node',
         name='slam_toolbox',
         parameters=[slam_params_file, 
-            {'use_sim_time': use_sim_time}]
+            {'use_sim_time': True}]
     )
     map_saver_server = Node(
         package='ros2_com',
@@ -35,8 +34,6 @@ def generate_launch_description():
    
 
     return launch.LaunchDescription([      
-    DeclareLaunchArgument(name='use_sim_time', default_value='true',
-                                        description='Flag to enable use_sim_time'), 
     DeclareLaunchArgument(name='slam_params_file',
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node'),
     DeclareLaunchArgument(name='bag_file', description='Full path to bag file'), 
