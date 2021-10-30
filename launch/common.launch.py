563b0fac9effd1f4d5c950039215f9f36fce41f5
Valts Masāns <valts114@gmail.com>
2021-10-30 15:08:00 +0000

    uncommented point2block

--- launch/common.launch.py
+++ launch/common.launch.py
@@ -199,7 +199,7 @@ def generate_launch_description():
     ld.add_action(robot_state_publisher_node)
     ld.add_action(pose_listener_node)
     ld.add_action(odom_publisher_node)
-    # ld.add_action(point2block_node)
+    ld.add_action(point2block_node)
     ld.add_action(crash_event_hand)
 
     if lidar_params['lidar_model'] == 'ouster':
